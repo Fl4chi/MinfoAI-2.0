ed9a28a7f77d24feb0b1d4fd738b2a275ebe04b4
Fl4chi <149245996+Fl4chi@users.noreply.github.com>
2025-11-15 16:22:24 +0100

    Add main bot file (src/index.js) with MongoDB and handlers

---
+++ src/index.js
@@ -0,0 +1,51 @@
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const mongoose = require('mongoose');
+const path = require('path');
+require('dotenv').config();
+const logger = require('./utils/logger');
+const commandHandler = require('./handlers/commandHandler');
+const eventHandler = require('./handlers/eventHandler');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMembers
+    ]
+});
+
+client.commands = new Collection();
+
+// Connessione MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    logger.info('\u2705 Connesso a MongoDB con successo!');
+}).catch(err => {
+    logger.error('\u274c Errore connessione MongoDB:', err);
+    process.exit(1);
+});
+
+// Carica handlers
+commandHandler(client);
+eventHandler(client);
+
+// Login bot
+client.login(process.env.BOT_TOKEN).catch(err => {
+    logger.error('\u274c Errore login bot:', err);
+    process.exit(1);
+});
+
+// Gestione errori globali
+process.on('unhandledRejection', error => {
+    logger.error('\u274c Unhandled promise rejection:', error);
+});
+
+process.on('uncaughtException', error => {
+    logger.error('\u274c Uncaught exception:', error);
+    process.exit(1);
+});
+
+module.exports = client;
