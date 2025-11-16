7a8fec5f70f357fca3a82356888767e634d8d610
Fl4chi <149245996+Fl4chi@users.noreply.github.com>
2025-11-16 14:34:49 +0100

    Integrate errorLogger into commandHandler - FINAL INTEGRATION BATCH

--- src/handlers/commandHandler.js
+++ src/handlers/commandHandler.js
@@ -1,22 +1,24 @@
 const fs = require('fs');
 const path = require('path');
-const logger = require('../utils/logger');
+const errorLogger = require('../utils/errorLogger');
 
 module.exports = async (client) => {
   const commandsPath = path.join(__dirname, '..', 'commands');
-  
+
   const commandFolders = fs.readdirSync(commandsPath).filter(f => {
     return fs.statSync(path.join(commandsPath, f)).isDirectory();
   });
 
   for (const folder of commandFolders) {
     const files = fs.readdirSync(path.join(commandsPath, folder)).filter(f => f.endsWith('.js'));
-    
+
     for (const file of files) {
       const command = require(path.join(commandsPath, folder, file));
       if (command.data && command.execute) {
         client.commands.set(command.data.name, command);
-        logger.success(`Loaded command: ${command.data.name}`);
+        errorLogger.logInfo('INFO', `Loaded command: ${command.data.name}`, 'COMMAND_LOADED');
+      } else {
+        errorLogger.logWarn('WARNING', `Command file ${file} missing data or execute`, 'COMMAND_LOAD_INVALID');
       }
     }
   }
